ed27e053589c3624b44f9b31edf6f9a292ee5fc4
= <nooralden.12.n@gmail.com>
2026-03-18 19:56:01 +0200

    fix: update link to include trailing slash

--- src/app/page.js
+++ src/app/page.js
@@ -18,8 +18,7 @@ export default function Home() {
           <h1>Khalaile Nour</h1>
         
             Here your app should come....
-            <Link href="/system">מעבר ל-System</Link>
-
+            <Link href="/system/">מעבר ל-System</Link>
         </div>
       </main>
     </div>
